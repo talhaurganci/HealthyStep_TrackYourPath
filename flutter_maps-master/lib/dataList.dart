f6b123b44c630722f4af05b05e9d623feab431e3
talhaurganci <65421059+talhaurganci@users.noreply.github.com>
2021-11-10 00:54:47 +0300

    flutter_maps-master
    FlutterMaps Application

---
+++ flutter_maps-master/lib/dataList.dart
@@ -0,0 +1,15 @@
+class DataList{
+  int index;
+  String uId;
+  String runName;
+  double latitude;
+  double longitude;
+
+   DataList(int index, String uId, String runName, double latitude, double longitude) {
+   this.index = index;
+   this.uId = uId;
+   this.runName = runName;
+   this.latitude = latitude;
+   this.longitude = longitude;
+  }
+}
